b810f8554febc5d4f796e9fde9bac7b009514591
PhilSoGooood <peelhw@gmail.com>
2022-05-18 00:27:05 +0900

    다양한 연관관계 매핑

--- jpa-basic/src/main/java/hellojpa/Team.java
+++ jpa-basic/src/main/java/hellojpa/Team.java
@@ -1,13 +1,9 @@
 package hellojpa;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Team {
@@ -17,9 +13,6 @@ public class Team {
 	@Column(name = "TEAM_ID")
 	private Long id;
 	private String name;
-
-	@OneToMany(mappedBy = "team")
-	private List<Member> members = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -35,13 +28,5 @@ public class Team {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public List<Member> getMembers() {
-		return members;
-	}
-
-	public void setMembers(List<Member> members) {
-		this.members = members;
 	}
 }
