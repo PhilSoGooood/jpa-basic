e30d2092557c4adb1051a472584b5981bf853fac
PhilSoGooood <peelhw@gmail.com>
2022-05-21 23:48:40 +0900

    즉시 로딩과 지연로딩

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -21,7 +22,7 @@ public class Member extends BaseEntity {
 	// @Column(name = "TEAM_ID")
 	// private Long teamId;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
 
